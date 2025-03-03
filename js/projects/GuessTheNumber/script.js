09cb37055f4e19890a241864292db89d58d6b583
Jit Mukherjee <133637574+GTAJIT@users.noreply.github.com>
2025-03-04 00:52:25 +0530

    /

---
+++ js/projects/GuessTheNumber/script.js
@@ -0,0 +1,9 @@
+const Min = 1
+const Max = 100
+const randomNum = Math.floor(Math.random()*(Max-Min+1)+Min)
+// console.log(randomNum);
+const Click = document.getElementById('submit')
+Click.addEventListener('click', (i)=>{
+    const Input = parseInt(document.getElementById('input').value)
+    
+})
